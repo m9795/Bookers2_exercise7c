df50473d148b6865b794c67742d2586a47733fdf
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-30 22:35:29 +0900

    [Add]Groupモデルimage定義変更,グループ作成ページ表示,groupsコントローラ追記,Usereditページ部分テンプレート化

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,9 @@
 class Group < ApplicationRecord
   has_many :users
   has_many :group_users
-  has_one_attached :group_image
+  has_one_attached :profile_image
 
-  def get_group_image
-    (group_image.attached?) ? group_image : 'no_image.jpg'
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 end
