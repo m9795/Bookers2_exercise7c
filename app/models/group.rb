0a3c26302a364d0fb52b6dccff47f8fc13e61d16
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-30 21:45:05 +0900

    [Add]アソシエーションの追加,groupにimageメソッド作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,9 @@
 class Group < ApplicationRecord
+  has_many :users
+  has_many :group_users
+  has_one_attached :group_image
+
+  def get_group_image
+    (group_image.attached?) ? group_image : 'no_image.jpg'
+  end
 end
