f284da69baede5b7c978ef5475404b831929fc89
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-30 22:02:16 +0900

    [Add]ユーザ一覧ページにグループ作成,一覧のリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'groups/new'
-  get 'groups/show'
-  get 'groups/index'
-  get 'groups/edit'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :groups, only: [:new, :create, :show, :index, :edit, :update]
 end
