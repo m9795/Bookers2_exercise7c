b988e908202397e4984b28450d70bb2ab39ad749
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-30 22:55:59 +0900

    [Add]コントローラ編集,グループ作成保存機能,一覧表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,13 +2,27 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-
+  
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    @group.save
+    redirect_to groups_path
+  end
+  
   def show
   end
 
   def index
+    @book = Book.new
+    @groups = Group.all
   end
 
   def edit
+  end
+  
+  private
+  def group_params
+    params.require(:group).permit(:name, :introduction, :profile_image)
   end
 end
